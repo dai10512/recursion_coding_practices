785ac5a8ab9f796c00c5d62e4da17455a538cf42
Daisuke Osanai <dsk.osanai@gmail.com>
2022-04-16 00:55:47 +0900

    no comment

---
+++ coding_practices/basic_computer_sciece/medium_class/countWords.js
@@ -0,0 +1,31 @@
+function countWords(sentence){
+    // 関数を完成させてください
+    count = 0
+    for(let i = 0; i < sentence.length; i++){
+        if(sentence[i] == ' '){
+            count ++
+        }
+    }
+    return count +1
+}
+
+
+// 単語のカウント
+// very easy
+// Jeffers は本の感想を共有できる web プラットフォームを開発しています。そこでは投稿の際、文章中にいくつ単語があるか見れるようになっています。英単語のみで構成された文章 sentence が与えられるので、そこに含まれる単語数をカウントする、countWords という関数を作成してください。
+
+// 関数の入出力例
+
+// 入力のデータ型： string sentence
+
+// 出力のデータ型： integer
+
+// countWords("I would rather be a bird than a fish") --> 9
+
+// countWords("He liked to play with words in the bathtub") --> 9
+
+// countWords("word") --> 1
+
+// countWords("For writers a random sentence can help them get their creative juices flowing") --> 13
+
+// countWords("When I go to business school I remember I lost my key of house") --> 14
---
+++ coding_practices/basic_computer_sciece/medium_class/factorial.js
@@ -0,0 +1,28 @@
+function factorial(n){
+    // 関数を完成させてください
+    count = 1
+    for(let i = 0; i < n; i ++){
+        count = count * (i + 1) 
+    }
+    return count
+}
+
+// 階乗
+// easy
+// 自然数 n が与えられるので、1 × 2 × 3 × ... × n を計算する factorial という関数を再帰を使って作成してください。
+
+// 関数の入出力例
+
+// 入力のデータ型： integer n
+
+// 出力のデータ型： long
+
+// factorial(2) --> 2
+
+// factorial(3) --> 6
+
+// factorial(5) --> 120
+
+// factorial(10) --> 3628800
+
+// factorial(15) --> 1307674368000
